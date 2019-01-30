a31306b3761efd96e0c0cd6a3dc8fd63d8b6deb0
justinmusti <mustiatwork@gmail.com>
2019-01-30 15:56:02 -0800

    Created Conversation and Message models
    Created the base structure for messages.
    Created migrations for above models.

--- jmsg/core/models.py
+++ jmsg/core/models.py
@@ -1,7 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from uuid import uuid4
+from django.conf import settings
 
 
 class User(AbstractUser):
     pass
 
+
+class Conversation(models.Model):
+
+    id = models.UUIDField(primary_key=True, unique=True, null=False, blank=False, default=uuid4)
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False, blank=False,
+                               related_name='sender')
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False, blank=False,
+                                 related_name='receiver')
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Message(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, null=False, blank=False, default=uuid4)
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, null=False, blank=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False, blank=False)
+    text = models.TextField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
