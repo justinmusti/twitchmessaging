7bb0e09bd81f89979ae2905bffd5877dfb402fc7
justinmusti <mustiatwork@gmail.com>
2019-02-01 12:40:44 -0800

    Verified backend database engine is Postgres.
    Updated env variable names to reflect docker recommendation.
    Added a new APP_STAGE `staging`.

--- jmsg/jmsg/settings/__init__.py
+++ jmsg/jmsg/settings/__init__.py
@@ -1,10 +1,14 @@
 import os
 
+
 APP_STAGE = os.environ.get('APP_STAGE', 'development')
 
 if APP_STAGE == 'production':
     from .production import *
-elif APP_STAGE in ['development', 'staging']:
+elif APP_STAGE in ['development']:
     from .development import *
+elif APP_STAGE == 'staging':
+    from .staging import *
+
 else:
     raise Exception('Unexpected APP_STAGE environment.')
---
+++ jmsg/jmsg/settings/staging.py
@@ -0,0 +1,16 @@
+from .default import *
+import os
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'postgress'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgress'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'root'),
+        'PORT': os.environ.get('POSTGRES_PORT', 5432),
+    }
+}
